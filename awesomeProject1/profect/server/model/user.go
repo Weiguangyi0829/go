cc8699b36a4c8ac3973ca84d2c9391a227866fd4
MrVWY <365273231@qq.com>
2019-09-17 16:03:28 +0800

    awesomeProject1

--- awesomeProject1/profect/server/model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-//Define the struct of a user
-
-type User struct {
-	UserId int         `json:"user_id"`
-	UserPwd string	   `json:"user_pwd"`
-	UserName string    `json:"user_name"`
-}
